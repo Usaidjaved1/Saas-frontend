e75152da0be3ac75dac1398f1e8da6f226b1f809
Usaidjaved1 <uccucoral@gmail.com>
2026-04-14 01:43:29 +0500

    some minor changes

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { motion } from "framer-motion";
-import "../public.css";
+
 
 function PublicProfile() {
   const { username } = useParams();
